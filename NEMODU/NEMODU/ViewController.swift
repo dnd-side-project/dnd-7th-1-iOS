4961a628730bec06b13455337c846c3980c0662c
yunkyung-Hwang <dudhkd98@naver.com>
2022-07-28 22:28:47 +0900

    project init

---
+++ NEMODU/NEMODU/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NEMODU
+//
+//  Created by 황윤경 on 2022/07/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
