32928453f2b7231c7510f27683ad3a4c3a8be668
yunkyung-Hwang <dudhkd98@naver.com>
2022-07-31 23:41:13 +0900

    [ADD]: info-location Auth & background modes

--- NEMODU/NEMODU/Screen/Map/ViewController/MapVC.swift
+++ NEMODU/NEMODU/Screen/Map/ViewController/MapVC.swift
@@ -11,11 +11,26 @@ import RxGesture
 import RxSwift
 import SnapKit
 import Then
+import MapKit
+import CoreLocation
 
 class MapVC: BaseViewController {
-    private var tmp = UILabel()
+    private var mapView = MKMapView()
         .then {
-            $0.text = "MAP"
+            $0.mapType = .standard
+            $0.setUserTrackingMode(.followWithHeading, animated: true)
+            $0.showsUserLocation = true
+            $0.isZoomEnabled = true
+            $0.showsTraffic = false
+        }
+    
+    private var locationManager = CLLocationManager()
+        .then {
+            $0.desiredAccuracy = kCLLocationAccuracyBest
+            $0.allowsBackgroundLocationUpdates = true
+            $0.requestAlwaysAuthorization()
+            $0.startUpdatingLocation()
+            $0.activityType = .fitness
         }
     
     override func viewDidLoad() {
@@ -24,12 +39,12 @@ class MapVC: BaseViewController {
     
     override func configureView() {
         super.configureView()
-        configureLabel()
+        configureMap()
     }
     
     override func layoutView() {
         super.layoutView()
-        labelLayout()
+        configureLayout()
     }
     
     override func bindInput() {
@@ -45,17 +60,24 @@ class MapVC: BaseViewController {
 // MARK: - Configure
 
 extension MapVC {
-    private func configureLabel() {
-        view.addSubview(tmp)
+    private func configureMap() {
+        view.addSubview(mapView)
+        locationManager.delegate = self
+        mapView.delegate = self
+        
+        // 현재 위치로 이동
+        _ = goLocation(latitudeValue: locationManager.location?.coordinate.latitude ?? 0,
+                       longtudeValue: locationManager.location?.coordinate.longitude ?? 0,
+                       delta: 0.01)
     }
 }
 
 // MARK: - Layout
 
 extension MapVC {
-    private func labelLayout() {
-        tmp.snp.makeConstraints {
-            $0.center.equalToSuperview()
+    private func configureLayout() {
+        mapView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
         }
     }
 }
@@ -69,5 +91,32 @@ extension MapVC {
 // MARK: - Output
 
 extension MapVC {
+    
+}
+
+// MARK: - CLLocationManagerDelegate
+extension MapVC: CLLocationManagerDelegate {
+    /// 위도, 경도, 스팬(영역 폭)을 입력받아 지도에 표시
+    func goLocation(latitudeValue: CLLocationDegrees,
+                           longtudeValue: CLLocationDegrees,
+                           delta span: Double) -> CLLocationCoordinate2D {
+        let pLocation = CLLocationCoordinate2DMake(latitudeValue, longtudeValue)
+        let spanValue = MKCoordinateSpan(latitudeDelta: span, longitudeDelta: span)
+        let pRegion = MKCoordinateRegion(center: pLocation, span: spanValue)
+        mapView.setRegion(pRegion, animated: true)
+        return pLocation
+    }
+    
+    /// 특정 위도와 경도에 핀 설치하고 핀에 타이틀과 서브 타이틀의 문자열 표시
+    func setAnnotation(latitudeValue: CLLocationDegrees, longitudeValue: CLLocationDegrees, delta span: Double, title strTitle: String, subtitle strSubtitle: String) {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = goLocation(latitudeValue: latitudeValue, longtudeValue: longitudeValue, delta: span)
+        annotation.title = strTitle
+        annotation.subtitle = strSubtitle
+        mapView.addAnnotation(annotation)
+    }
+}
+
+extension MapVC: MKMapViewDelegate {
     
 }
