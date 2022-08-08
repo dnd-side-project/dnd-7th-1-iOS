139ed47e8b14822a19a1983a08577bbcb7589767
yunkyung-Hwang <dudhkd98@naver.com>
2022-08-08 19:05:30 +0900

    [ADD]: filter bottomSheet 컴포넌트 추가

--- NEMODU/NEMODU/Global/Extension/UIButton+.swift
+++ NEMODU/NEMODU/Global/Extension/UIButton+.swift
@@ -30,4 +30,21 @@ extension UIButton {
         self.setImage(defaultImage, for: .normal)
         self.setImage(selectedImage, for: .selected)
     }
+    
+    /// 버튼 이미지를 상단에, 버튼 타이틀을 하단에 배치해주는 함수
+    func centerVertically(spacing: CGFloat = 6.0) {
+        guard
+            let image = self.imageView?.image,
+            let titleLabel = self.titleLabel,
+            let titleText = titleLabel.text else {
+            return
+        }
+
+        let titleSize = titleText.size(withAttributes: [
+            NSAttributedString.Key.font: titleLabel.font as Any
+        ])
+
+        titleEdgeInsets = UIEdgeInsets(top: spacing, left: -image.size.width, bottom: -image.size.height, right: 0)
+        imageEdgeInsets = UIEdgeInsets(top: -(titleSize.height + spacing), left: 0, bottom: 0, right: -titleSize.width)
+    }
 }
--- NEMODU/NEMODU/Screen/Map/ViewController/BottomSheet/MapFilterBottomSheet.swift
+++ NEMODU/NEMODU/Screen/Map/ViewController/BottomSheet/MapFilterBottomSheet.swift
@@ -6,9 +6,8 @@
 //
 
 import UIKit
-import RxCocoa
-import RxGesture
 import RxSwift
+import RxCocoa
 import SnapKit
 import Then
 import DynamicBottomSheet
@@ -16,32 +15,80 @@ import DynamicBottomSheet
 class MapFilterBottomSheet: DynamicBottomSheetViewController {
     private let viewBar = UIView()
         .then {
-            $0.backgroundColor = UIColor.lightGray
+            $0.backgroundColor = UIColor.systemGray4
             $0.layer.cornerRadius = 2
         }
     
     private let sheetTitle = UILabel()
         .then {
             $0.text = "지도 필터"
-            $0.font = UIFont.systemFont(ofSize: 16)
+            $0.font = UIFont.systemFont(ofSize: 17)
             $0.textColor = UIColor.black
         }
     
     private let titleMessage = UILabel()
         .then {
-            $0.text = "이번주의 기록만 보여집니다"
+            $0.text = "이번주(월~일)의 기록만 보여집니다."
             $0.font = UIFont.systemFont(ofSize: 14)
             $0.textColor = UIColor.lightGray
         }
     
-    // TODO: - 여백값 디자인 확정 이후 수정
-    let top = 36
-    let leading = 24
-    let trailing = -24
+    private let btnStackView = UIStackView()
+        .then {
+            $0.axis = .horizontal
+            $0.spacing = 56
+            $0.alignment = .fill
+            $0.distribution = .fillEqually
+        }
+    
+    private let showMyBlocksBtn = UIButton()
+        .then {
+            $0.setTitle("나의 기록 보기", for: .normal)
+            $0.setTitleColor(.black, for: .normal)
+            $0.titleLabel?.font = UIFont.systemFont(ofSize: 15)
+            $0.toggleButtonImage(defaultImage: UIImage(named: "hideBlocks")!,
+                                 selectedImage: UIImage(named: "showBlocks")!)
+            $0.centerVertically(spacing: 8)
+        }
+    
+    private let showFriendsBtn = UIButton()
+        .then {
+            $0.setTitle("친구 보기", for: .normal)
+            $0.setTitleColor(.black, for: .normal)
+            $0.titleLabel?.font = UIFont.systemFont(ofSize: 15)
+            $0.toggleButtonImage(defaultImage: UIImage(named: "hideFriends")!,
+                                 selectedImage: UIImage(named: "showFriends")!)
+            $0.centerVertically(spacing: 8)
+        }
+    
+    private let separatorLine = UIView()
+        .then {
+            $0.backgroundColor = UIColor.systemGray5
+        }
+    
+    private let locationPermissionTitle = UILabel()
+        .then {
+            $0.text = "내 위치 공개"
+            $0.font = UIFont.systemFont(ofSize: 16)
+        }
+    
+    private let locationPermissionMessage = UILabel()
+        .then {
+            $0.text = "친구들에게 보이기"
+            $0.font = UIFont.systemFont(ofSize: 15)
+        }
+    
+    private let locationPermissionToggleBtn = UISwitch()
+        .then {
+            $0.onTintColor = UIColor.main
+        }
+    
+    private let bag = DisposeBag()
     
     override func configureView() {
         super.configureView()
         configureLayout()
+        bindBtn()
     }
 }
 
@@ -49,24 +96,100 @@ class MapFilterBottomSheet: DynamicBottomSheetViewController {
 
 extension MapFilterBottomSheet {
     func configureLayout() {
-        contentView.addSubviews([viewBar, sheetTitle, titleMessage])
+        contentView.addSubviews([viewBar,
+                                 sheetTitle,
+                                 titleMessage,
+                                 btnStackView,
+                                 separatorLine,
+                                 locationPermissionTitle,
+                                 locationPermissionMessage,
+                                 locationPermissionToggleBtn])
+        btnStackView.addArrangedSubview(showMyBlocksBtn)
+        btnStackView.addArrangedSubview(showFriendsBtn)
+        
+        contentView.snp.makeConstraints {
+            $0.height.equalTo(340)
+        }
         
         viewBar.snp.makeConstraints {
-            $0.width.equalTo(56)
+            $0.width.equalTo(32)
             $0.height.equalTo(4)
             $0.centerX.equalToSuperview()
             $0.top.equalToSuperview().offset(10)
         }
         
         sheetTitle.snp.makeConstraints {
-            $0.top.equalToSuperview().offset(top)
-            $0.leading.equalToSuperview().offset(leading)
-            $0.bottom.equalToSuperview().offset(-333)
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(viewBar.snp.bottom).offset(8)
         }
         
         titleMessage.snp.makeConstraints {
-            $0.centerY.equalTo(sheetTitle.snp.centerY)
-            $0.leading.equalTo(sheetTitle.snp.trailing).offset(20)
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(sheetTitle.snp.bottom).offset(8)
         }
+        
+        btnStackView.snp.makeConstraints {
+            $0.top.equalTo(titleMessage.snp.bottom).offset(16)
+            $0.leading.equalToSuperview().offset(55)
+            $0.trailing.equalToSuperview().offset(-55)
+            $0.height.equalTo(88)
+        }
+        
+        separatorLine.snp.makeConstraints {
+            $0.height.equalTo(1)
+            $0.top.equalTo(btnStackView.snp.bottom).offset(48)
+            $0.leading.equalToSuperview().offset(16)
+            $0.trailing.equalToSuperview().offset(-16)
+        }
+        
+        locationPermissionTitle.snp.makeConstraints {
+            $0.top.equalTo(separatorLine.snp.bottom).offset(16)
+            $0.leading.equalToSuperview().offset(16)
+        }
+        
+        locationPermissionMessage.snp.makeConstraints {
+            $0.top.equalTo(locationPermissionTitle.snp.bottom).offset(16)
+            $0.leading.equalTo(locationPermissionTitle)
+        }
+        
+        locationPermissionToggleBtn.snp.makeConstraints {
+            $0.centerY.equalTo(locationPermissionMessage)
+            $0.trailing.equalToSuperview().offset(-16)
+        }
+    }
+}
+
+// MARK: - bind
+
+extension MapFilterBottomSheet {
+    func bindBtn() {
+        // 나의 기록 보기 버튼
+        showMyBlocksBtn.rx.tap
+            .asDriver()
+            .drive(onNext: { [weak self] _ in
+                guard let self = self else { return }
+                self.showMyBlocksBtn.isSelected.toggle()
+                // TODO: - 서버 연결 & 지도 UI 변경
+            })
+            .disposed(by: bag)
+
+        // 친구 보기 버튼
+        showFriendsBtn.rx.tap
+            .asDriver()
+            .drive(onNext: { [weak self] _ in
+                guard let self = self else { return }
+                self.showFriendsBtn.isSelected.toggle()
+                // TODO: - 서버 연결 & 지도 UI 변경
+            })
+            .disposed(by: bag)
+        
+        // 내 위치 공개 버튼
+        locationPermissionToggleBtn.rx.isOn
+            .changed
+            .withUnretained(self)
+            .subscribe(onNext: { owner, status in
+                // TODO: - 서버 연결
+            })
+            .disposed(by: bag)
     }
 }
