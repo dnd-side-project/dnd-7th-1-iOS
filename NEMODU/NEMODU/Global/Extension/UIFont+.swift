885c8283c0b95fddf03075ecd0e2c74896fba1bf
yunkyung-Hwang <dudhkd98@naver.com>
2022-08-09 16:02:32 +0900

    [ADD]: 폰트 추가

---
+++ NEMODU/NEMODU/Global/Extension/UIFont+.swift
@@ -0,0 +1,34 @@
+//
+//  UIFont+.swift
+//  NEMODU
+//
+//  Created by 황윤경 on 2022/08/09.
+//
+
+import UIKit
+
+extension UIFont {
+    class func PretendardBlack(size: CGFloat) -> UIFont {
+        return UIFont(name: "Pretendard-Black", size: size)!
+    }
+    
+    class func PretendardBold(size: CGFloat) -> UIFont {
+        return UIFont(name: "Pretendard-Bold", size: size)!
+    }
+    
+    class func PretendardExtraBold(size: CGFloat) -> UIFont {
+        return UIFont(name: "Pretendard-ExtraBold", size: size)!
+    }
+    
+    class func PretendardMedium(size: CGFloat) -> UIFont {
+        return UIFont(name: "Pretendard-Medium", size: size)!
+    }
+    
+    class func PretendardRegular(size: CGFloat) -> UIFont {
+        return UIFont(name: "Pretendard-Regular", size: size)!
+    }
+    
+    class func PretendardSemiBold(size: CGFloat) -> UIFont {
+        return UIFont(name: "Pretendard-SemiBold", size: size)!
+    }
+}
