d260675dcd0e4bb53fb13cabb453691ee8aa9c7f
yunkyung-Hwang <dudhkd98@naver.com>
2022-08-09 19:17:01 +0900

    [ADD]: 폰트 변수 추가

--- NEMODU/NEMODU/Global/Extension/UIFont+.swift
+++ NEMODU/NEMODU/Global/Extension/UIFont+.swift
@@ -8,6 +8,70 @@
 import UIKit
 
 extension UIFont {
+    // Heading
+    @nonobjc class var number1: UIFont{
+        PretendardExtraBold(size: 48)
+    }
+    
+    @nonobjc class var number2: UIFont{
+        PretendardSemiBold(size: 32)
+    }
+    
+    @nonobjc class var title1: UIFont{
+        PretendardSemiBold(size: 28)
+    }
+    
+    @nonobjc class var title2: UIFont{
+        PretendardSemiBold(size: 22)
+    }
+    
+    @nonobjc class var title3SB: UIFont{
+        PretendardSemiBold(size: 20)
+    }
+    
+    @nonobjc class var title3M: UIFont{
+        PretendardMedium(size: 20)
+    }
+    
+    @nonobjc class var headline1: UIFont{
+        PretendardSemiBold(size: 17)
+    }
+    
+    @nonobjc class var headline2: UIFont{
+        PretendardSemiBold(size: 13)
+    }
+    
+    // Paragraph
+    @nonobjc class var body1: UIFont{
+        PretendardRegular(size: 17)
+    }
+    
+    @nonobjc class var body2: UIFont{
+        PretendardRegular(size: 16)
+    }
+    
+    @nonobjc class var body3: UIFont{
+        PretendardRegular(size: 15)
+    }
+    
+    @nonobjc class var body4: UIFont{
+        PretendardRegular(size: 13)
+    }
+    
+    @nonobjc class var caption1: UIFont{
+        PretendardRegular(size: 12)
+    }
+    
+    @nonobjc class var caption2R: UIFont{
+        PretendardRegular(size: 11)
+    }
+    
+    @nonobjc class var caption2M: UIFont{
+        PretendardMedium(size: 11)
+    }
+    
+    
+    // Basic
     class func PretendardBlack(size: CGFloat) -> UIFont {
         return UIFont(name: "Pretendard-Black", size: size)!
     }
