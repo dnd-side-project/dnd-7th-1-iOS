bf21afd2a70af295b928d6d46abe7d6d6ba05437
Kim HeeJae <iowa329@gmail.com>
2022-08-15 01:09:51 +0900

    [CHORE] TabBar 아이콘 이미지 추가

--- NEMODU/NEMODU/Screen/TabBar/NEMODUTBC.swift
+++ NEMODU/NEMODU/Screen/TabBar/NEMODUTBC.swift
@@ -15,7 +15,7 @@ class NEMODUTBC: UITabBarController {
         super.viewDidLoad()
         
         setTabBar()
-        setTabIndex(index: 1)
+        setStartTabIndex(index: 0)
     }
     
 }
@@ -24,40 +24,40 @@ class NEMODUTBC: UITabBarController {
 
 extension NEMODUTBC {
     
-    private func makeTabVC(vc: UIViewController, tabBarTitle: String, tabBarImage: String, tabBarSelectedImage: String) -> UIViewController {
-        
-        let tab = vc
-        tab.tabBarItem = UITabBarItem(title: tabBarTitle, image: UIImage(named: tabBarImage), selectedImage: UIImage(named: tabBarSelectedImage))
-        tab.tabBarItem.imageInsets = UIEdgeInsets(top: -0.5, left: -0.5, bottom: -0.5, right: -0.5)
-        return tab
-    }
-    
     private func setTabBar() {
-        let challengeRankingTab = makeTabVC(vc: ChallengeRankingVC(), tabBarTitle: "챌린지/랭킹", tabBarImage: "", tabBarSelectedImage: "")
-        let mapTab = makeTabVC(vc: MapVC(), tabBarTitle: "지도", tabBarImage: "", tabBarSelectedImage: "")
-        let mypageTab = makeTabVC(vc: MypageVC(), tabBarTitle: "MY", tabBarImage: "", tabBarSelectedImage: "")
-        
         // 탭바 스타일 설정
         tabBar.backgroundColor = .white
         tabBar.isTranslucent = false
         
-        tabBar.tintColor = .black
-        tabBar.unselectedItemTintColor = .lightGray
+        tabBar.tintColor = .main
+        tabBar.unselectedItemTintColor = .gray300
         
         tabBar.layer.shadowColor = UIColor.lightGray.cgColor
         tabBar.layer.shadowOffset = CGSize(width: 0, height: 0)
         tabBar.layer.shadowOpacity = 0.3
         
         // 탭 구성
+        let challengeRankingTab = makeTabVC(vc: ChallengeRankingVC(), tabBarTitle: "챌린지/랭킹", tabBarImage: "TabBarIcon/challengeRanking" , tabBarSelectedImage: "TabBarIcon/challengeRanking_fill")
+        let mapTab = makeTabVC(vc: MapVC(), tabBarTitle: "지도", tabBarImage: "TabBarIcon/map", tabBarSelectedImage: "TabBarIcon/map_fill")
+        let mypageTab = makeTabVC(vc: MypageVC(), tabBarTitle: "MY", tabBarImage: "TabBarIcon/mypage", tabBarSelectedImage: "TabBarIcon/mypage_fill")
+        
         let tabs =  [challengeRankingTab, mapTab, mypageTab]
         
         // VC에 루트로 설정
         self.setViewControllers(tabs, animated: false)
     }
     
-    /// tabBar의 선택된 탭을 지정하는 함수
-    func setTabIndex(index: Int) {
+    func setStartTabIndex(index: Int) {
         self.selectedIndex = index
     }
+    
+    private func makeTabVC(vc: UIViewController, tabBarTitle: String, tabBarImage: String, tabBarSelectedImage: String) -> UIViewController {
+        let tab = vc
+        tab.tabBarItem = UITabBarItem(title: tabBarTitle, image: UIImage(named: tabBarImage)?.withRenderingMode(.alwaysOriginal), selectedImage: UIImage(named: tabBarSelectedImage)?.withRenderingMode(.alwaysOriginal))
+        tab.tabBarItem.imageInsets = UIEdgeInsets(top: -0.5, left: -0.5, bottom: -0.5, right: -0.5)
+        
+        return tab
+    }
+    
     
 }
