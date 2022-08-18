f12b7f7a42124e6f3dee8f16a9f69be5e37337b8
Kim HeeJae <iowa329@gmail.com>
2022-08-18 23:57:40 +0900

    [FEAT]: 챌린지 내역 유형별 선택 CollectioView 메뉴바 제작

---
+++ NEMODU/NEMODU/Screen/ChallengeRanking/ChallengeRankingMenuBar/ChallengeListTypeMenuBarCVC.swift
@@ -0,0 +1,64 @@
+//
+//  ChallengeListTypeMenuBarCVC.swift
+//  NEMODU
+//
+//  Created by Kim HeeJae on 2022/08/18.
+//
+
+import UIKit
+import Then
+import SnapKit
+
+class ChallengeListTypeMenuBarCVC: MenuBarCVC {
+    
+    // MARK: - UI components
+    
+    // MARK: - Life Cycle
+    
+    // MARK: - Functions
+    
+    override func configureCell() {
+        _ = menuTitle
+            .then {
+                $0.font = .title3SB
+                $0.textColor = .gray400
+            }
+    }
+    
+    // MARK: - Override Function
+    
+    /// Set button highlighted status
+    
+    override var isHighlighted: Bool {
+        didSet {
+            if isSelected == true {
+                _ = menuTitle.then {
+                    $0.font = .title3SB
+                    $0.textColor = .gray900
+                }
+            } else {
+                _ = menuTitle.then {
+                    $0.font = .title3SB
+                    $0.textColor = .gray400
+                }
+            }
+        }
+    }
+    
+    override var isSelected: Bool {
+        didSet {
+            if isSelected == true {
+                _ = menuTitle.then {
+                    $0.font = .title3SB
+                    $0.textColor = .gray900
+                }
+            } else {
+                _ = menuTitle.then {
+                    $0.font = .title3SB
+                    $0.textColor = .gray400
+                }
+            }
+        }
+    }
+
+}
