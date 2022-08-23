05966dcb22ddf0073f569feb9bac31ca395f1e93
yunkyung-Hwang <dudhkd98@naver.com>
2022-08-24 03:46:22 +0900

    [CHORE]: APISession postRequest parameter Optional로 변경

--- NEMODU/NEMODU/Global/Network/APIService.swift
+++ NEMODU/NEMODU/Global/Network/APIService.swift
@@ -11,7 +11,7 @@ import RxSwift
 protocol APIService {
     func getRequest<T: Decodable>(with urlResource: urlResource<T>) -> Observable<Result<T, APIError>>
     
-    func postRequest<T: Decodable>(with urlResource: urlResource<T>, param: Parameters) -> Observable<Result<T, APIError>>
+    func postRequest<T: Decodable>(with urlResource: urlResource<T>, param: Parameters?) -> Observable<Result<T, APIError>>
     
     func postRequestWithImage<T: Decodable>(with urlResource: urlResource<T>, param: Parameters, image: UIImage, method: HTTPMethod) -> Observable<Result<T, APIError>>
 }
