8f240c3c7d71891ccb17a4843d2b95f4fe5992aa
Kim HeeJae <iowa329@gmail.com>
2022-09-12 22:03:47 +0900

    [ADD]: 내부 패팅값 지정 가능한 커스텀 Label 구성

---
+++ NEMODU/NEMODU/Global/Utils/PaddingLabel.swift
@@ -0,0 +1,24 @@
+//
+//  PaddingLabel.swift
+//  NEMODU
+//
+//  Created by Kim HeeJae on 2022/08/25.
+//
+
+import UIKit
+
+class PaddingLabel: UILabel {
+
+    /// 라벨 안에 padding 값을 설정
+    var edgeInset: UIEdgeInsets = .zero
+
+    override func drawText(in rect: CGRect) {
+        let insets = UIEdgeInsets.init(top: edgeInset.top, left: edgeInset.left, bottom: edgeInset.bottom, right: edgeInset.right)
+        super.drawText(in: rect.inset(by: insets))
+    }
+
+    override var intrinsicContentSize: CGSize {
+        let size = super.intrinsicContentSize
+        return CGSize(width: size.width + edgeInset.left + edgeInset.right, height: size.height + edgeInset.top + edgeInset.bottom)
+    }
+}
