a1ef4c7f9c15d586e4e76f5d3295cdb13858de60
yunkyung-Hwang <dudhkd98@naver.com>
2022-09-20 00:30:33 +0900

    [FEAT]: 토큰 만료 처리

--- NEMODU/NEMODU/Global/Network/AuthInterceptor.swift
+++ NEMODU/NEMODU/Global/Network/AuthInterceptor.swift
@@ -6,8 +6,11 @@
 //
 
 import Alamofire
+import RxSwift
 
 class AuthInterceptor: RequestInterceptor {
+    let bag = DisposeBag()
+    
     func adapt(_ urlRequest: URLRequest, for session: Session, completion: @escaping (Result<URLRequest, Error>) -> Void) {
         guard let accessToken = UserDefaults.standard.string(forKey: UserDefaults.Keys.accessToken) else { return }
         
@@ -23,11 +26,22 @@ class AuthInterceptor: RequestInterceptor {
             completion(.doNotRetryWithError(error))
             return
         }
-        // accessToken이 401 만료되었을 경우 refreshToken으로 갱신 요청
-        // refreshToken도 만료 시 로그인 화면으로 이동 throw
-//        DispatchQueue.main.async {
-//            guard let ad = UIApplication.shared.delegate as? AppDelegate else { return }
-//            ad.window?.rootViewController = LoginNC()
-//        }
+        
+        // accessToken이 401 만료되었을 경우 토큰 갱신 요청 후 retry
+        AuthAPI.shared.renewalToken()
+            .withUnretained(self)
+            .subscribe(onNext: { owner, result in
+                switch result {
+                case .failure(let error):
+                    completion(.doNotRetryWithError(error))
+                    DispatchQueue.main.async {
+                        guard let ad = UIApplication.shared.delegate as? AppDelegate else { return }
+                        ad.window?.rootViewController = LoginNC()
+                    }
+                case .success(_):
+                    completion(.retry)
+                }
+            })
+            .disposed(by: bag)
     }
 }
