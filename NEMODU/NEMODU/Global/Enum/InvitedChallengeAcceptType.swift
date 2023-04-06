31d7b6787d95ec44285dd4be2c4a39f9d02e4aeb
Kim HeeJae <iowa329@gmail.com>
2023-04-06 21:41:28 +0900

    [CHORE] : 챌린지 종류 판별 rawValue 대문자(upper-case)로 변경

--- NEMODU/NEMODU/Global/Enum/InvitedChallengeAcceptType.swift
+++ NEMODU/NEMODU/Global/Enum/InvitedChallengeAcceptType.swift
@@ -9,9 +9,10 @@ import UIKit
 
 /// 초대받은 챌린지 수락상태 타입 3가지(주최자, 대기중, 수락완료)
 enum InvitedChallengeAcceptType: String {
-    case master = "Master"
-    case wait = "Wait"
-    case progress = "Progress"
+    case master
+    case wait
+    case progress
+    case reject
 }
 
 extension InvitedChallengeAcceptType {
@@ -23,6 +24,16 @@ extension InvitedChallengeAcceptType {
             return "대기중"
         case .progress:
             return "수락 완료"
+        case .reject:
+            return "거절"
         }
+    }
+}
+
+// MARK: - Custom String Convertible
+
+extension InvitedChallengeAcceptType: CustomStringConvertible {
+    var description: String {
+        rawValue.uppercased()
     }
 }
