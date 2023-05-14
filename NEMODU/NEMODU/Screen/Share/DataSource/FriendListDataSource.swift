38a9c2d996fe6923231489cbb2ecb7e497024fcd
yunkyung-Hwang <dudhkd98@naver.com>
2023-05-14 20:57:32 +0900

    [ADD]: FriendListDataSource 추가

---
+++ NEMODU/NEMODU/Screen/Share/DataSource/FriendListDataSource.swift
@@ -0,0 +1,22 @@
+//
+//  FriendListDataSource.swift
+//  NEMODU
+//
+//  Created by 황윤경 on 2023/05/14.
+//
+
+import Foundation
+import RxDataSources
+
+struct FriendListDataSource {
+    var section: Int
+    var items: [Item]
+}
+
+extension FriendListDataSource: SectionModelType {
+    typealias Item = FriendsInfo
+    init(original: FriendListDataSource, items: [Item]) {
+        self = original
+        self.items = items
+    }
+}
