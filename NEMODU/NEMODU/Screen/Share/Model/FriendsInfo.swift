06d71ca1d1c7a9e4671c667161db2373a2b9ca5b
yunkyung-Hwang <dudhkd98@naver.com>
2023-05-14 19:27:42 +0900

    [ADD]: FriendsInfo responseModel 추가

---
+++ NEMODU/NEMODU/Screen/Share/Model/FriendsInfo.swift
@@ -0,0 +1,15 @@
+//
+//  FriendsInfo.swift
+//  NEMODU
+//
+//  Created by 황윤경 on 2023/05/14.
+//
+
+import Foundation
+
+struct FriendsInfo: Codable {
+    let nickname: String
+    let picturePath: String
+    let kakaoName: String?
+    let status: String?
+}
