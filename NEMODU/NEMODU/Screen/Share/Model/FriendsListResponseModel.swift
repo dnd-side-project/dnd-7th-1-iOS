5b4d24ef2575a6a45b15f2ff1e9eafdc8762bca4
yunkyung-Hwang <dudhkd98@naver.com>
2023-05-16 00:32:38 +0900

    [RENAME]: FriendListResponseModel Share>Model로 이동, Info -> FriendDefaultInfo로 변경

---
+++ NEMODU/NEMODU/Screen/Share/Model/FriendsListResponseModel.swift
@@ -0,0 +1,15 @@
+//
+//  FriendsListResponseModel.swift
+//  NEMODU
+//
+//  Created by Kim HeeJae on 2022/11/01.
+//
+
+import Foundation
+import Alamofire
+
+struct FriendsListResponseModel: Codable {
+    let infos: [FriendDefaultInfo]
+    let isLast: Bool
+    let size: Int
+}
