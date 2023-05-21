30d1f97157fd3675b78e5a61bf324c1a4e951fa0
yunkyung-Hwang <dudhkd98@naver.com>
2023-05-21 18:50:22 +0900

    [ADD]: DateType 및 날짜, 시간 관련 함수 추가

--- NEMODU/NEMODU/Global/Enum/DateType.swift
+++ NEMODU/NEMODU/Global/Enum/DateType.swift
@@ -8,23 +8,35 @@
 import Foundation
 
 enum DateType {
+    /// yyyy-MM-dd'T'HH:mm:ss
     case withTime
+    /// yyyy-MM-dd
     case hyphen
+    /// yyyy-MM-dd'T'
     case withT
+    /// yyyy-MM-dd HH:mm:ss
     case withBlank
+    /// 24시간제 시간
+    case hourClock24
+    /// 12시간제 시간
+    case hourClock12
 }
 
 extension DateType {
     var dateFormatter: String {
         switch self {
-        case .hyphen:
-            return "yyyy-MM-dd"
         case .withTime:
             return "yyyy-MM-dd'T'HH:mm:ss"
+        case .hyphen:
+            return "yyyy-MM-dd"
         case .withT:
             return "yyyy-MM-dd'T'"
         case .withBlank:
             return "yyyy-MM-dd HH:mm:ss"
+        case .hourClock24:
+            return "HH:mm"
+        case .hourClock12:
+            return "hh:mm"
         }
     }
 }
