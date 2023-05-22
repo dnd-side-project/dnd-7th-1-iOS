b00f652a2b0e30ed3260516889d383b5d2c12136
yunkyung-Hwang <dudhkd98@naver.com>
2023-05-22 17:01:44 +0900

    [ADD]: AuthAPI 네트워크 에러 연결

--- NEMODU/NEMODU/Global/Network/AuthAPI.swift
+++ NEMODU/NEMODU/Global/Network/AuthAPI.swift
@@ -28,6 +28,11 @@ extension AuthAPI {
     /// [POST] 헤더에 kakaoAccessToken을 붙여 회원가입을 요청하는 메서드
     func signupRequest<T: Decodable>(with urlResource: urlResource<T>, param: Parameters) -> Observable<Result<T, APIError>> {
         Observable<Result<T, APIError>>.create { observer in
+            if !NetworkMonitor.shared.isConnected {
+                observer.onNext(.failure(.networkDisconnected))
+                return Disposables.create()
+            }
+            
             let headers: HTTPHeaders = [
                 "Content-Type": "application/json"
             ]
@@ -61,6 +66,11 @@ extension AuthAPI {
     /// [GET] 헤더에 소셜 토큰을 붙여 소셜 로그인을 요청하는 메서드
     func socialLoginRequest<T: Decodable>(with urlResource: urlResource<T>, token: String) -> Observable<Result<T, APIError>> {
         Observable<Result<T, APIError>>.create { observer in
+            if !NetworkMonitor.shared.isConnected {
+                observer.onNext(.failure(.networkDisconnected))
+                return Disposables.create()
+            }
+            
             let headers: HTTPHeaders = [
                 "Content-Type": "application/json",
                 "Authorization": token
@@ -91,6 +101,11 @@ extension AuthAPI {
     /// [POST] 헤더에 소셜 토큰을 붙여 네모두 로그인을 요청하는 메서드
     func loginRequest<T: Decodable>(with urlResource: urlResource<T>, token: String, param: Parameters) -> Observable<Result<T, APIError>> {
         Observable<Result<T, APIError>>.create { observer in
+            if !NetworkMonitor.shared.isConnected {
+                observer.onNext(.failure(.networkDisconnected))
+                return Disposables.create()
+            }
+            
             let headers: HTTPHeaders = [
                 "Content-Type": "application/json",
                 "Authorization": token
@@ -171,6 +186,11 @@ extension AuthAPI {
     func checkNickname<T>(with urlResource: urlResource<T>) -> Observable<Result<T, APIError>> where T : Decodable {
         
         Observable<Result<T, APIError>>.create { observer in
+            if !NetworkMonitor.shared.isConnected {
+                observer.onNext(.failure(.networkDisconnected))
+                return Disposables.create()
+            }
+            
             let headers: HTTPHeaders = [
                 "Content-Type": "application/json"
             ]
@@ -201,6 +221,11 @@ extension AuthAPI {
     func editProfile<T: Decodable>(with urlResource: urlResource<T>, param: Parameters, image: UIImage) -> Observable<Result<T, APIError>> {
         
         Observable<Result<T, APIError>>.create { observer in
+            if !NetworkMonitor.shared.isConnected {
+                observer.onNext(.failure(.networkDisconnected))
+                return Disposables.create()
+            }
+            
             let headers: HTTPHeaders = [
                 "Content-Type": "application/json"
             ]
