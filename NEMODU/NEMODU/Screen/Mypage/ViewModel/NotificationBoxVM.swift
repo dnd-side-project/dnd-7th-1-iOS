59fee8fc001d0bf11faa676f2625b1b7ced0d320
Kim HeeJae <iowa329@gmail.com>
2023-05-24 21:06:30 +0900

    [ADD] : 알림함 서버연결 파일 구성

---
+++ NEMODU/NEMODU/Screen/Mypage/ViewModel/NotificationBoxVM.swift
@@ -0,0 +1,103 @@
+//
+//  NotificationBoxVM.swift
+//  NEMODU
+//
+//  Created by Kim HeeJae on 2023/05/18.
+//
+
+import Foundation
+
+import RxCocoa
+import RxSwift
+
+protocol NotificationBoxVMOuput: Lodable {
+    var notificationList: PublishRelay<NotificationBoxResponseModel> { get }
+}
+
+final class NotificationBoxVM: BaseViewModel {
+    var apiSession: APIService = APISession()
+    let apiError = PublishSubject<APIError>()
+    
+    var bag = DisposeBag()
+    
+    var input = Input()
+    var output = Output()
+    
+    // MARK: - Input
+    
+    struct Input {}
+    
+    // MARK: - Output
+    
+    struct Output: NotificationBoxVMOuput {
+        var loading = BehaviorRelay<Bool>(value: false)
+        var notificationList = PublishRelay<NotificationBoxResponseModel>()
+//        var isSuccessUpdateNotificationSetting = PublishRelay<Bool>()
+    }
+    
+    // MARK: - Init
+    
+    init() {
+        bindInput()
+        bindOutput()
+    }
+    
+    deinit {
+        bag = DisposeBag()
+    }
+}
+
+// MARK: - Input
+
+extension NotificationBoxVM: Input {
+    func bindInput() {}
+}
+
+// MARK: - Output
+
+extension NotificationBoxVM: Output {
+    func bindOutput() {}
+}
+
+// MARK: - Networking
+
+extension NotificationBoxVM {
+    func getUserNotificationList() {
+        guard let nickname = UserDefaults.standard.string(forKey: UserDefaults.Keys.nickname) else { return }
+        let path = "noti?nickname=\(nickname)"
+        let resource = urlResource<NotificationBoxResponseModel>(path: path)
+        
+        apiSession.getRequest(with: resource)
+            .withUnretained(self)
+            .subscribe(onNext: { owner, result in
+                switch result {
+                case .failure(let error):
+                    owner.apiError.onError(error)
+                case .success(let data):
+                    owner.output.notificationList.accept(data)
+                }
+            })
+            .disposed(by: bag)
+    }
+    
+//    func updateNotificationSetting(notificationType: NotificationCategoryType) {
+//        guard let nickname = UserDefaults.standard.string(forKey: UserDefaults.Keys.nickname) else { return }
+//
+//        let path = "user/filter/notification"
+//        let resource = urlResource<Bool>(path: path)
+//
+//        apiSession.postRequest(with: resource, param: UpdateNotificationSettingRequestModel(nickname: nickname, notification: notificationType.identifier).param)
+//            .withUnretained(self)
+//            .subscribe(onNext: { owner, result in
+//                switch result {
+//                case .failure(let error):
+//                    owner.apiError.onError(error)
+//                    owner.output.isSuccessUpdateNotificationSetting.accept(false)
+//                case .success(let data):
+//                    owner.output.isSuccessUpdateNotificationSetting.accept(data)
+//                }
+//            })
+//            .disposed(by: bag)
+//    }
+}
+
